0990f93b7fb11ccf136e3cca475832f2f3c08f01
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-10-04 01:59:09 -0500

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -18,3 +18,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Servidor corriendo en puerto 3000");
 });
+
+app.use("/api/user", userRouter);
