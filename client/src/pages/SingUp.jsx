71ba3f4bf4361f26a1318226f0f3c44fe5f670a4
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-10-05 00:13:03 -0500

    complete sing-up page functionality

--- client/src/pages/SingUp.jsx
+++ client/src/pages/SingUp.jsx
@@ -1,16 +1,77 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SingUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/sign-up", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.sucess === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  //console.log(formData);
+
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className='text-3xl text-center font-semibold my-7'>
-        Inscribirse
-      </h1>
-      <form className='flex flex-col gap-4'>
-        <input type='text' placeholder='Usuario' className='border p-3 rounded-lg' id='username' />
-        <input type='email' placeholder='Email' className='border p-3 rounded-lg' id='email' />
-        <input type='password' placeholder='Clave' className='border p-3 rounded-lg' id='password' />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Incribirse</button>
+      <h1 className="text-3xl text-center font-semibold my-7">Inscribirse</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Usuario"
+          className="border p-3 rounded-lg"
+          id="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Clave"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Cargando..." : "inscribirse"}
+        </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Tienes una cuenta?</p>
@@ -18,6 +79,7 @@ export default function SingUp() {
           <span className="text-blue-700">Ingresar</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-  )
+  );
 }
