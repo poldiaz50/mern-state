d6f9723f0269b3ff4ed90bb42aa94e0f9238b154
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-10-21 00:46:19 -0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,41 @@
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Acerca de PolDiazEstate</h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis
+        blandit felis. Nulla tempor aliquam quam eget tempor. Maecenas volutpat
+        tincidunt lectus, id pulvinar turpis. Maecenas aliquet viverra sagittis.
+        Suspendisse accumsan augue at augue tristique sodales. 
+      </p>
+      <br/>
+      <p className="mb-4 text-slate-700">
+        Pellentesque blandit velit sed lorem suscipit, ut pharetra mauris
+        molestie. Morbi faucibus risus id lacus ornare, sed molestie orci
+        eleifend. Donec nec est ut urna malesuada vehicula. Fusce aliquet, lacus
+        ac auctor rutrum, felis felis tincidunt quam, quis interdum arcu erat
+        semper eros. Ut et nisi a ex iaculis pretium facilisis vel justo.
+        Aliquam pretium metus pharetra leo mollis viverra. Proin blandit
+        vulputate lectus, et fermentum dolor semper eget. Nullam vel nisl in
+        nisl vehicula finibus in mattis nulla. Sed convallis gravida posuere.
+        Curabitur volutpat varius nulla, id facilisis nulla consequat ut. 
+      </p>
+      <br/>
+      <p className="mb-4 text-slate-700">
+        Vestibulum pulvinar velit tempus, accumsan libero vel, dignissim purus.
+        Duis et iaculis lectus, eget molestie enim. Fusce vitae turpis congue
+        ipsum ornare commodo. Cras non congue enim, sit amet posuere erat. Etiam
+        et dolor ut velit pharetra pretium. Aenean vitae mauris vel tortor
+        gravida pulvinar. Sed massa augue, hendrerit a arcu ut, porttitor
+        convallis tortor. Donec tempus ultrices ex vel efficitur. Curabitur
+        sollicitudin elit non sapien maximus tempor. Nulla consectetur mi nec
+        vulputate pharetra. Duis sit amet maximus turpis. In hac habitasse
+        platea dictumst. Mauris rutrum nec ligula id dignissim. Cras accumsan,
+        mi a rutrum sollicitudin, sapien nulla placerat arcu, nec maximus nibh
+        risus ut augue. In consequat tortor augue, nec cursus est viverra
+        pretium. Quisque auctor, tellus vel congue varius, diam felis
+        scelerisque odio, in posuere leo magna ac
+      </p>
+    </div>
+  );
 }
